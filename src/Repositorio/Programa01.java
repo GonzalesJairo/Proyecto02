e73e75dd20e9264a3aa19bdb55b7ab04287a249d
GonzalesJairo <2021018211@unfv.edu.pe>
2024-05-04 22:51:56 -0500

    Segundo Avance del codigo
    Segundo avance del codigo

--- src/Repositorio/Programa01.java
+++ src/Repositorio/Programa01.java
@@ -2,6 +2,10 @@ package Repositorio;
 
 public class Programa01{
     public static void main(String [] args){
-        System.out.println("Hola mundo cruel !!!");
+        int n1=12,n2=90,mayor=0;
+        if(n1>n2)
+            System.out.println("El mayor es: "+n1);
+        else
+            System.out.println("El mayor es: "+n2);
     }
 }
